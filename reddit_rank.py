8d648d75cdb31b3b5bd1dabbe7142131a1368baa
Pedro Howat <pedro@instant.com.br>
2017-03-16 21:19:03 -0300

    initial commit

---
+++ reddit_rank.py
@@ -0,0 +1,23 @@
+import requests
+import requests.auth
+import pprint
+import praw
+from flask import Flask
+from os import environ
+app = Flask(__name__)
+reddit = praw.Reddit(
+    client_id=environ["REDDIT_CLIENT_ID"],
+    client_secret=environ["REDDIT_CLIENT_SECRET"],
+    password=environ["REDDIT_PASSWORD"],
+    user_agent=environ["REDDIT_USER_AGENT"],
+    username=environ["REDDIT_USERNAME"])
+
+
+@app.route('/')
+def hello_world():
+    sublist = ""
+    subs = sorted(reddit.user.subreddits(), 
+            key=lambda x: x.subscribers, reverse=True)
+    for sub in subs:
+        sublist += "{} - {} <br>".format(sub.display_name, sub.subscribers)
+    return sublist
